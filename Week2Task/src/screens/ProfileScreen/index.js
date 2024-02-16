41a677dfa7eb9a2a1e7c1072d730ca5125e33ef2
parthi198811 <156941783+parthi198811@users.noreply.github.com>
2024-02-16 17:01:55 +0000

    Test Image Picker

--- Week2Task/src/screens/ProfileScreen/index.js
+++ Week2Task/src/screens/ProfileScreen/index.js
@@ -1,16 +1,37 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/dist/FontAwesome6';
 import IconM from 'react-native-vector-icons/dist/MaterialIcons';
 import {useUserContext} from '@contexts/UserContext';
 import styles from './styles';
+import ImageCropPicker from 'react-native-image-crop-picker';
+import {Image} from 'react-native-animatable';
 
 const ProfileScreen = () => {
   const {state} = useUserContext();
   const loggedInUser = state.loggedInUser;
 
+  const [profileImage, setProfileImage] = useState(null);
+
   return (
     <View style={styles.container}>
+      <Button
+        title="ImagePicker"
+        onPress={() => {
+          ImageCropPicker.openPicker({
+            width: 300,
+            height: 400,
+            cropping: true,
+          }).then(image => {
+            setProfileImage(image.sourceURL);
+            console.log(image);
+          });
+        }}
+      />
+      <Image
+        source={{uri: profileImage}}
+        style={{width: 100, height: 100, backgroundColor: 'grey'}}
+      />
       <Icon name="circle-user" color={'black'} size={100} />
       <Text style={styles.headerText}>{loggedInUser.name}</Text>
       <View style={{flexDirection: 'row'}}>
